c6ee35a1d9e6c9afca0f641488cf46881abc2c59
teh-BMW <fattolo170@gmail.com>
2017-03-08 17:52:05 +0100

    commented Room; next target: add item count, redo item types

--- al0ne/src/com/al0ne/Room.java
+++ al0ne/src/com/al0ne/Room.java
@@ -2,31 +2,37 @@ package com.al0ne;
 
 import com.al0ne.Items.Item;
 import com.al0ne.Items.Prop;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Created by BMW on 28/01/2017.
  *
  * a Room is:
- * an interactables, all obj that one can interact with
+ * a props, all props in the room
  * an items, all objects you can pickup
- * a description
- * a name
- * an exits, hashmap of direction - roomid
- * an lockedDirection, hashmap of doorName - direction
+ * a description of the room
+ * a name of the room
+ * an exits, HashMap of direction - roomid
+ * an lockedDirection, HashMap of doorName - direction
  *
  */
 public class Room {
 
-    private HashMap<String, Prop> props;
-    private HashMap<String, Item> items;
+
+    //room description
     private String description;
+    //room name
     private String name;
+    //room ID
     private String id;
+    //maps direction - room ID
     private HashMap<String, String> exits;
+    //maps door ID - direction
     private HashMap<String, String> lockedDirections;
+    //maps propName - Prop
+    private HashMap<String, Prop> props;
+    //maps itemName - Item
+    private HashMap<String, Item> items;
 
     public Room(String id, String name, String description) {
         this.id=id;
@@ -42,6 +48,19 @@ public class Room {
         return props;
     }
 
+    public HashMap<String, Item> getItems() {
+        return items;
+    }
+
+    public HashMap<String, String> getExits() {
+        return exits;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    //prints props in the room
     public void printProps(){
         if (props.size()!=0){
             for (Prop prop : props.values()) {
@@ -50,10 +69,7 @@ public class Room {
         }
     }
 
-    public HashMap<String, Item> getItems() {
-        return items;
-    }
-
+    //prints items in the room
     public void printItems(){
         if (items.size()!=0){
             System.out.println("You can see:");
@@ -63,55 +79,11 @@ public class Room {
         }
     }
 
-    public HashMap<String, String> getExits() {
-        return exits;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
     private void printDescription(){
         System.out.println(description);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getId(){
-        return id;
-    }
-
-    public void printName() {
-        System.out.println(name);
-    }
-
-    public void addProp(Prop prop) {
-        this.props.put(prop.getID(), prop);
-    }
-
-    public void addItem(Item item) {
-        this.items.put(item.getID(), item);
-    }
-
-    public void addExit(String exit, String roomid) {
-        this.exits.put(exit, roomid);
-    }
-
-    public void printRoom(){
-        System.out.println(name);
-        printDescription();
-        printProps();
-        printItems();
-        printDirections();
-        System.out.println();
-    }
-
-    public void lockDirection(String direction, String idDoor){
-        lockedDirections.put(idDoor, direction);
-    }
-
+    //prints available travel directions that are not locked
     public void printDirections(){
         boolean first=true;
 
@@ -133,11 +105,39 @@ public class Room {
         }
     }
 
+    //this function prints every time a room is discovered
+    //// TODO: 08/03/2017 maybe shortened version? once visited once
+    public void printRoom(){
+        System.out.println(name);
+        printDescription();
+        printProps();
+        printItems();
+        printDirections();
+        System.out.println();
+    }
+
+    public void addProp(Prop prop) {
+        this.props.put(prop.getID(), prop);
+    }
+
+    public void addItem(Item item) {
+        this.items.put(item.getID(), item);
+    }
+
+    public void addExit(String exit, String roomid) {
+        this.exits.put(exit, roomid);
+    }
+
+    //this function locks direction behind doorID
+    public void lockDirection(String direction, String idDoor){
+        lockedDirections.put(idDoor, direction);
+    }
 
     public void unlockDirection(String nameDoor){
         lockedDirections.remove(nameDoor);
     }
 
+    //checks if the current direction is locked
     public boolean isLocked(String direction){
         for (String s : lockedDirections.values()){
             System.out.println(s);
