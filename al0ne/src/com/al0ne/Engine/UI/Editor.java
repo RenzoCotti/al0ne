475912921b8719b144dd428a027127fc5c9435f5
teh-BMW <fattolo170@gmail.com>
2017-05-18 13:15:54 +0200

    added npc ui added enemy UI

--- al0ne/src/com/al0ne/Engine/UI/Editor.java
+++ al0ne/src/com/al0ne/Engine/UI/Editor.java
@@ -25,15 +25,15 @@ public class Editor {
 
         Tab props = createProp();
 
-        Tab characters = new Tab();
-        characters.setText("Characters");
-        characters.setClosable(false);
+        Tab npc = createNPC();
+
+        Tab enemy = createEnemy();
 
         Tab other = new Tab();
         other.setText("Other");
         other.setClosable(false);
 
-        editor.getTabs().addAll(rooms, items, props, characters, other);
+        editor.getTabs().addAll(rooms, items, props, npc, enemy, other);
         VBox temp = new VBox();
         temp.getChildren().add(editor);
         return temp;
@@ -94,7 +94,203 @@ public class Editor {
 
         items.setContent(itemContent);
 
+        Button create = new Button("Create Prop");
+        itemContent.add(create, 0, 12);
+
         return items;
+    }
+
+    public static Tab createNPC(){
+        Tab items = new Tab();
+        items.setText("NPC");
+        items.setClosable(false);
+
+        GridPane itemContent = new GridPane();
+
+        Label createNewProp = new Label("Create new NPC:");
+        createNewProp.setStyle("-fx-font-weight: bold");
+        itemContent.add(createNewProp, 0, 0);
+
+        TextField nameText = new TextField();
+        nameText.setPromptText("Bob");
+        Label nameLabel = new Label("Name:");
+        itemContent.add(nameLabel, 0, 1);
+        itemContent.add(nameText, 1, 1);
+
+        TextField shortDesc = new TextField();
+        shortDesc.setPromptText("a clever-looking man");
+        Label shortDescLabel = new Label("Short Description\n(optional):");
+        itemContent.add(shortDescLabel, 0, 2);
+        itemContent.add(shortDesc, 1, 2);
+
+        TextArea descText = new TextArea();
+        descText.setPrefWidth(100);
+        descText.setPrefHeight(50);
+        Label descLabel = new Label("Description:");
+        descText.setPromptText("A chubby man, wearing a scarf and a silly hat.");
+        itemContent.add(descLabel, 0, 3);
+        itemContent.add(descText, 1, 3);
+
+        TextArea introText = new TextArea();
+        introText.setPrefWidth(100);
+        introText.setPrefHeight(50);
+        Label introLabel = new Label("Intro dialog:");
+        introText.setPromptText("Hi, I'm Bob. How are you?");
+        itemContent.add(introLabel, 0, 4);
+        itemContent.add(introText, 1, 4);
+
+
+        Spinner<Integer> maxHealth = new Spinner<>();
+        maxHealth.setEditable(true);
+        maxHealth.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50, 0, 1));
+        Label healthLabel = new Label("Max Health:");
+        itemContent.add(healthLabel, 0, 5);
+        itemContent.add(maxHealth, 1, 5);
+
+        Spinner<Integer> attack = new Spinner<>();
+        attack.setEditable(true);
+        attack.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 0, 1));
+        Label attackLabel = new Label("Attack:");
+        itemContent.add(attackLabel, 0, 6);
+        itemContent.add(attack, 1, 6);
+
+        Spinner<Integer> dex = new Spinner<>();
+        dex.setEditable(true);
+        dex.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 0, 1));
+        Label dexLabel = new Label("Dexterity:");
+        itemContent.add(dexLabel, 0, 7);
+        itemContent.add(dex, 1, 7);
+
+        Spinner<Integer> armor = new Spinner<>();
+        armor.setEditable(true);
+        armor.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20, 0, 1));
+        Label armorLabel = new Label("Armor:");
+        itemContent.add(armorLabel, 0, 8);
+        itemContent.add(armor, 1, 8);
+
+        Spinner<Integer> damage = new Spinner<>();
+        damage.setEditable(true);
+        damage.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20, 0, 1));
+        Label damageLabel = new Label("Damage:");
+        itemContent.add(damageLabel, 0, 9);
+        itemContent.add(damage, 1, 9);
+
+        ToggleButton isShopkeeper = new RadioButton("Is shopkeeper?");
+        itemContent.add(isShopkeeper, 1, 10);
+
+        Button addItem = new Button("Add Item");
+        itemContent.add(addItem, 0, 11);
+
+        Button addSubject = new Button("Add Subject");
+        itemContent.add(addSubject, 0, 12);
+
+        Button create = new Button("Create NPC");
+        itemContent.add(create, 0, 15);
+
+        itemContent.setPadding(new Insets(5, 10, 5, 5));
+
+        items.setContent(itemContent);
+
+        return items;
+    }
+
+
+    public static Tab createEnemy(){
+        Tab enemy = new Tab();
+        enemy.setText("Enemy");
+        enemy.setClosable(false);
+
+        GridPane enemyContent = new GridPane();
+
+        Label createNewProp = new Label("Create new Enemy:");
+        createNewProp.setStyle("-fx-font-weight: bold");
+        enemyContent.add(createNewProp, 0, 0);
+
+        TextField nameText = new TextField();
+        nameText.setPromptText("wolf");
+        Label nameLabel = new Label("Name:");
+        enemyContent.add(nameLabel, 0, 1);
+        enemyContent.add(nameText, 1, 1);
+
+        TextField shortDesc = new TextField();
+        shortDesc.setPromptText("a wolf");
+        Label shortDescLabel = new Label("Short Description\n(optional):");
+        enemyContent.add(shortDescLabel, 0, 2);
+        enemyContent.add(shortDesc, 1, 2);
+
+        TextArea descText = new TextArea();
+        descText.setPrefWidth(100);
+        descText.setPrefHeight(50);
+        Label descLabel = new Label("Description:");
+        descText.setPromptText("A ferocious wolf, with black fur.");
+        enemyContent.add(descLabel, 0, 3);
+        enemyContent.add(descText, 1, 3);
+
+        TextArea introText = new TextArea();
+        introText.setPrefWidth(100);
+        introText.setPrefHeight(50);
+        Label introLabel = new Label("Resistances:");
+        introText.setPromptText("TODO");
+        enemyContent.add(introLabel, 0, 4);
+        enemyContent.add(introText, 1, 4);
+
+
+        Spinner<Integer> maxHealth = new Spinner<>();
+        maxHealth.setEditable(true);
+        maxHealth.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 50, 0, 1));
+        Label healthLabel = new Label("Max Health:");
+        enemyContent.add(healthLabel, 0, 5);
+        enemyContent.add(maxHealth, 1, 5);
+
+        Spinner<Integer> attack = new Spinner<>();
+        attack.setEditable(true);
+        attack.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 0, 1));
+        Label attackLabel = new Label("Attack:");
+        enemyContent.add(attackLabel, 0, 6);
+        enemyContent.add(attack, 1, 6);
+
+        Spinner<Integer> dex = new Spinner<>();
+        dex.setEditable(true);
+        dex.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 0, 1));
+        Label dexLabel = new Label("Dexterity:");
+        enemyContent.add(dexLabel, 0, 7);
+        enemyContent.add(dex, 1, 7);
+
+        Spinner<Integer> armor = new Spinner<>();
+        armor.setEditable(true);
+        armor.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20, 0, 1));
+        Label armorLabel = new Label("Armor:");
+        enemyContent.add(armorLabel, 0, 8);
+        enemyContent.add(armor, 1, 8);
+
+        Spinner<Integer> damage = new Spinner<>();
+        damage.setEditable(true);
+        damage.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20, 0, 1));
+        Label damageLabel = new Label("Damage:");
+        enemyContent.add(damageLabel, 0, 9);
+        enemyContent.add(damage, 1, 9);
+
+        TextArea status = new TextArea();
+        status.setPrefWidth(100);
+        status.setPrefHeight(50);
+        Label statusLabel = new Label("Inflicts status:");
+        status.setPromptText("TODO");
+        enemyContent.add(statusLabel, 0, 10);
+        enemyContent.add(status, 1, 10);
+
+        ToggleButton aggressive = new RadioButton("Aggressive?");
+        enemyContent.add(aggressive, 1, 11);
+
+        enemy.setContent(enemyContent);
+
+
+        Button create = new Button("Create Enemy");
+        enemyContent.add(create, 0, 15);
+
+        enemyContent.setPadding(new Insets(5, 10, 5, 5));
+
+
+        return enemy;
     }
 
     public static Tab createItem(){
@@ -104,13 +300,10 @@ public class Editor {
 
         GridPane itemContent = new GridPane();
 
-//        Label createNewItem = new Label("Create new item:");
-//        createNewItem.setStyle("-fx-font-weight: bold");
-
-        TextField idText = new TextField();
-        Label idLabel = new Label("ID:");
-        itemContent.add(idLabel, 0, 0);
-        itemContent.add(idText, 1, 0);
+        Label createNewItem = new Label("Create new item:");
+        createNewItem.setStyle("-fx-font-weight: bold");
+        itemContent.add(createNewItem, 0, 0);
+        //todo: check for existing item in the db having the same (material+name) name
 
         TextField nameText = new TextField();
         nameText.setPromptText("Bread loaf");
@@ -157,7 +350,17 @@ public class Editor {
         itemContent.add(canDrop, 1, 7);
         itemContent.add(isUnique, 1, 8);
 
-        itemContent.setPadding(new Insets(5, 10, 5, 5));
+        Label typeLabel = new Label("Type\n(optional):");
+        ObservableList<String> typeList = FXCollections.observableArrayList("Weapon", "Body Armor",
+                "Helmet", "Shield", "Food", "Book", "Coin", "Key");
+        ComboBox<String> typeDisplay = new ComboBox<>(typeList);
+        itemContent.add(typeLabel, 0, 9);
+        itemContent.add(typeDisplay, 1, 9);
+
+        Button create = new Button("Create Item");
+        itemContent.add(create, 0, 12);
+
+        itemContent.setPadding(new Insets(10, 10, 10, 10));
 
         items.setContent(itemContent);
 
@@ -202,6 +405,9 @@ public class Editor {
 
         Button addEntity = new Button("Add Entity");
         itemContent.add(addEntity, 0, 4);
+
+        Button create = new Button("Create Room");
+        itemContent.add(create, 0, 8);
 
 
         itemContent.setPadding(new Insets(5, 10, 5, 5));
